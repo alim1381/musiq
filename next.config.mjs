5f8e4f169e92db55509be0b1dc9ebe7fa4f1892a
alim1381 <ali.moaydi2020@gmail.com>
2024-03-05 17:45:23 +0330

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost", "musiq-roan.vercel.app"],
+    domains: ["localhost", "musiq-ten.vercel.app"],
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,
